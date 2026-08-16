ec73d1cfedb5ceac78f095d71b085b1aa20ce031
Dashiell Renaud <rend@google.com>
2026-08-16 23:39:53 +0200

    Connect live Firebase Realtime Database for cross-device synchronization

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,11 @@
 // Firebase Configuration for Roma Resident Portal (Gigliopad & Lily Terrace)
-// -----------------------------------------------------------------------------
-// To connect your real-time cloud database:
-// 1. Go to https://console.firebase.google.com/
-// 2. Create a project (or select an existing one)
-// 3. In the project dashboard, click "Add app" -> Web (</>) and copy your firebaseConfig
-// 4. In Build -> "Realtime Database", click "Create Database" (start in test mode or with read/write rules)
-// 5. Replace the placeholder values below with your credentials:
-
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBBMRrl1EAvoevzQXo4wdKu3YJlXp-gM60",
+  authDomain: "lily-terrace.firebaseapp.com",
+  databaseURL: "https://lily-terrace-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lily-terrace",
+  storageBucket: "lily-terrace.firebasestorage.app",
+  messagingSenderId: "428589079920",
+  appId: "1:428589079920:web:2071a0d813a85ccda343b5",
+  measurementId: "G-2DV1NMBCCV"
 };
